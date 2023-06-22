df26ed96a541ee9eb4a4630c2cb5a874e0e3fafa
jacqueline kenny <jkennyvasconcelos@hotmail.com>
2023-06-22 20:53:06 -0300

    salvando alterações (index.html)

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ $('form').on('submit',function(e) {
     e.preventDefault();
     const nomeDaTarefa = $('#Nome-tarefa').val();
     const novoItem = $('<li></li>');
-
     $(`
-        <a href="${nomeDaTarefa}">Nome da tarefa</a>
-    </div>
+    <a href="${nomeDaTarefa}">
+    pintar a casa
+    </a>
 `).appendTo(novoItem);
 $(novoItem).appendTo('ul');
 $('#Nome-tarefa').val('')
