b23e1de90ee7a8299d18af08cea646e129f18314
jacqueline kenny <jkennyvasconcelos@hotmail.com>
2023-08-07 23:30:25 -0300

    salvando alterações (main.js)

---
+++ js/main.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+    $('#Telefone').mask('(00) 0000-0000')
+    $('#CPF').mask('000.000.000-00')
+    $('#CEP').mask('00000-000')
+    $('form').validate({
+        rules:{
+            nome:{
+                required:true
+            },
+            email:{
+                required:true
+            },
+            telefone:{
+                required:true
+            },
+            CPF:{
+                required:true
+            },
+            endereço:{
+                required:true
+            },
+            CEP:{
+                required:true
+            },
+        },
+        messages: {
+            nome:'Por favor, insira o seu nome'
+        },
+        submitHandler:function(form){
+            console.log(form)
+        },
+        invalidHandler:function(evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids();
+            if(camposIncorretos) {
+                alert(`Exitem ${camposIncorretos} campos incorretos`)
+            }
+        },
+    })
+    })
