bcc29099bbcb17296676b8f5ba52e7209ca9f5c6
jacqueline kenny <jkennyvasconcelos@hotmail.com>
2023-08-20 23:57:19 -0300

    salvando alterações (index.html)

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
 $('form').on('submit',function(e) {
     e.preventDefault();
     const nomeDaTarefa = $('#Nome-tarefa').val();
-    const novoItem = $(`<li>${nomeDaTarefa}</li>`);
-    $(`
-    <a href="${nomeDaTarefa}">
-    </a>
-`).appendTo(novoItem);
-$(novoItem).appendTo('ul');
-$('#Nome-tarefa').val('');
-$('ul').on('click','li,checked',function(){
-$('.checked').main.css('text-decoration', 'line-through');
-})
+    if (nomeDaTarefa.trim()!==""){
+        const novoItem = $(`
+        <li>${nomeDaTarefa}</li>
+        `);
+        $('#listaNaMao').append(novoItem);
+        $('#Nome-tarefa').val('');
+    }
+});
 
-
-})
+$('#listaNaMao').on('click', 'li', function(){
+    $(this).toggleClass('checked');
+});
 
