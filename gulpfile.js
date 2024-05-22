239038d2aca2a25a64692cae8aaad0232b7a19c4
jacqueline kenny <jkennyvasconcelos@hotmail.com>
2024-05-21 21:56:03 -0300

    comit inicial

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
 
 function compilaSass() {
     return gulp.src ('./source/styles/main.scss')
-    .pipe(sass())
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+        outputStyle: 'compressed'
+    }))
+    .pipe(sourcemaps.write('./maps'))
     .pipe(gulp.dest('./build/styles'));
 }
 
@@ -29,3 +34,5 @@ function dizTchau() {
 exports.default = function() {
     gulp.watch('./source/styles/*.scss', {ignoreInitial: false }, gulp.series(compilaSass));
 }
+
+exports.compilaSass = compilaSass;
