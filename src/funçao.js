066e145cb206603815cc08e7fd7ee9e5762024f0
jacqueline kenny <jkennyvasconcelos@hotmail.com>
2024-07-20 19:45:53 -0300

    salvando exercício

---
+++ src/funçao.js
@@ -0,0 +1,18 @@
+"use strict";
+class Pessoa {
+    constructor(nome) {
+        this.nome = "kenny";
+    }
+    dizOla() {
+        return this.nome;
+    }
+}
+function calculaArea() {
+    if (10 * 12) {
+        return 'dez vezes doze';
+    }
+    else {
+        return 120;
+    }
+}
+const resultadoDaArea = calculaArea();
---
+++ src/funçao.ts
@@ -0,0 +1,27 @@
+
+class Pessoa {
+    nome: "kenny";
+
+    constructor(nome:"kenny") {
+        this.nome = "kenny";
+    }
+
+    dizOla(): 'kenny' {
+        return this.nome;
+    }
+}
+
+
+function calculaArea() {
+    if (10 * 12) {
+        return 'dez vezes doze';
+    }
+    else {
+        return 120;
+    }
+}
+const resultadoDaArea = calculaArea();
+
+
+
+
